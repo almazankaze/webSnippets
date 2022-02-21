a82ce6a9899f7953367f40e44374601e70381baf
John Almazan <almazankaze@live.com>
2022-02-20 19:00:39 -0600

    added submenu in desktop version

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,33 +1,5 @@
-let currDropDown = null;
-
 document
   .querySelector(".menu-btn")
   .addEventListener("click", () =>
     document.querySelector(".middle-nav").classList.toggle("show")
   );
-
-$(function () {
-  $(".drop-down").on("click", handleDropDown);
-});
-
-function handleDropDown() {
-  const dropdown = $(this).attr("id");
-
-  if (dropdown !== currDropDown) {
-    $("#" + dropdown)
-      .children(".sub-menu-box")
-      .addClass("showDropDown");
-
-    $("#" + currDropDown)
-      .children(".sub-menu-box")
-      .removeClass("showDropDown");
-
-    currDropDown = dropdown;
-  } else {
-    $("#" + currDropDown)
-      .children(".sub-menu-box")
-      .removeClass("showDropDown");
-
-    currDropDown = null;
-  }
-}
