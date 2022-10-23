7a006e613eb2ff395ca2b5f097462ce24b571e1d
John Almazan <almazankaze@live.com>
2022-10-23 15:49:17 -0500

    added a bit more to api example

--- api/main.js
+++ api/main.js
@@ -1,20 +1,53 @@
-const display = document.querySelector("#display-data");
-const btn = document.querySelector("button");
+const list = document.querySelector("#data-list");
 
-btn.addEventListener("click", () => {
-    getData();
-})
+// create a new li element
+const getNewListItem = (data) => {
+  // create li
+  const el = document.createElement("li");
+  el.classList.add("list-item");
+
+  // create elements that will go in li
+  const titleDiv = document.createElement("div");
+  titleDiv.classList.add("title-container");
+
+  const h4 = document.createElement("h4");
+  h4.innerText = data.attributes.canonicalTitle;
+
+  const btn = document.createElement("button");
+  btn.classList.add("list-btn");
+  btn.innerText = "see more";
+
+  titleDiv.appendChild(h4);
+  titleDiv.appendChild(btn);
+
+  // add elements to li
+  el.appendChild(titleDiv);
+
+  return el;
+};
 
 const getData = async () => {
+  try {
+    const res = await fetch("https://kitsu.io/api/edge/anime");
+    const obj = await res.json();
 
-    try {
+    const fragment = new DocumentFragment();
 
-        const res = await fetch("https://kitsu.io/api/edge/anime");
-        const obj = await res.json();
+    obj.data.forEach((item) => {
+      fragment.appendChild(getNewListItem(item));
+    });
 
-        display.innerHTML = `<h2>${obj.data[0].attributes.canonicalTitle}</h2>`
+    list.appendChild(fragment);
 
-    } catch(e) {
-        display.innerHTML = "<h2>Error getting data</h2>"
-    }
+    // add event to all buttons
+    document.querySelectorAll(".list-btn").forEach((btn, key) => {
+        btn.addEventListener("click", () => {
+            console.log(btn.previousSibling.innerText);
+        })
+    })
+  } catch (e) {
+    list.innerHTML = "<h2>Error getting data</h2>";
+  }
 };
+
+getData();
